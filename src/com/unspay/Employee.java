a83322676887aa6bbb6232d3a5e0bbacb052323c
dongting.mu <dongting.mu@MuDongting.unspay.group>
2018-01-17 17:20:01 +0800

    增加Emloyee实体

---
+++ src/com/unspay/Employee.java
@@ -0,0 +1,11 @@
+package com.unspay;
+
+public class Employee {
+
+	private static String username;
+	private static Integer age;
+
+	public static void main(String[] args) {
+		System.out.println(username + age);
+	}
+}
